9cc51ebf17ab051bd7e889aa49626be2788d1b50
Amrit-Gill <gill.amrit@outlook.com>
2016-06-11 13:03:40 +0200

    finishing up

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# ec2Manage
+# Introduction
+There are three scripts to list, start and stop aws-ec2 instances using python command line. 
+
+# Prerequisite
+- [Boto3](https://github.com/boto/boto3)
+- [Boto3 Documentation](http://boto3.readthedocs.io/en/latest)
+
+# Use
+## listEc2
+```python listEc2.py [arg]```   
+Single input argument accepted. Valid choices are [all, running, stopped].
+
+## startEc2
+```python startEc2.py [arg]```   
+Single input argument accepted. Valid choices is the instance id.
+
+## stopEc2
+```python stopEc2.py [arg]```   
+Single input argument accepted. Valid choices is the instance id.
---
+++ src/startEc2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import sys, boto3
+
+def verifyInArg(argv):
+	bool = True
+	if len(sys.argv) != 2:
+		bool = False
+		print('One input argument required | %d given | Valid choices is the instance id' %(len(sys.argv)-1))
+	return bool
+
+
+def startEc2(id):
+	ec2 = boto3.resource('ec2')
+	try:
+		instance = ec2.Instance(id)
+	except ValueError:
+		print("Oops!  The instance id is not valid")
+
+	if instance.state['Name'] in ["running"]:
+		print("Instance already running")
+	else:
+		instance.start()
+		print('Instance started')
+
+
+if __name__ == '__main__':
+	bool = verifyInArg(sys.argv)
+	if bool:
+		startEc2(sys.argv[1])
